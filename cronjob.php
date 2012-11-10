9868e3dfbba5c85d0d6f8523a27c3a79e23d7c20
Vitor Mattos <vitor.mattos@phprio.org>
2012-11-10 02:13:46 -0200

    remoção de teste

--- cronjob.php
+++ cronjob.php
@@ -1,9 +1,6 @@
 <?php
-exec("ps -ef | grep 'process.php' | grep -v grep | awk '{print $2}' | xargs kill -9");
+//exec("ps -ef | grep 'process.php' | grep -v grep | awk '{print $2}' | xargs kill -9");
 $run = exec("ps -ef | grep 'process.php' | grep -v grep");
 if($run) return;
 $path = realpath(dirname(__FILE__));
 pclose(popen("php $path/process.php > $path/log &", 'r'));
-
-
-
